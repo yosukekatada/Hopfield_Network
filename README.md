42bdba23423973fd0eb2db857d048b2820dbf002
Zero <Zeroto521@gmail.com>
2018-12-06 15:15:22 +0800

    :tada: :zap: :shit: fork source and change it
    Change Doc
    1. version up, py2 → py3
    2. remove re package
    3. remove function `mat2vec`
    4, add doc for each function

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-Strip off sunglasses! : Discrete Hopfield Network
-==============================
+# Hopfield Network
 
-## Summary ##
-This Python code is just a simple implementaion of discrete Hopfield Network (http://en.wikipedia.org/wiki/Hopfield_network). 
-Discrete Hopfield Network can learn/memorize patterns and remember/recover the patterns when the network feeds those with noises.
+This Python code is just a simple implementaion of discrete [Hopfield Network](http://en.wikipedia.org/wiki/Hopfield_network).
 
-## Example (What the code do) ##
-For example, you input a neat picture like this and get the network to memorize the pattern (My code automatically transform RGB Jpeg into black-white picture). 
+Discrete Hopfield Network can learn(memorize) patterns and remember(recover) the patterns when the network feeds those with noises.
+
+## Example (What the code do)
+
+For example, you input a neat picture like this and get the network to memorize the pattern (My code automatically transform RGB Jpeg into black-white picture).
 
 ![IMAGE](train_pics/yosuke.jpg)
 
@@ -14,60 +14,60 @@ After the network memorized it, you put the picture with noise(sunglasses) like 
 
 ![IMAGE](test_pics/yosuke_test.jpg)
 
-
 The network can strip off the sunglasses because the network remembers the former picture.
 
 ![IMAGE](after_1.jpeg)
 
+## Instructions
 
-# Instructions #
-##Input files##
+### Input files
+
 JPEG files like those in "train_pics".
 Network learns those pics as correct pics.
 If you want to add new pics, please put them in "train_pics" folder.
 
-##Test files##
-The pictures with sunglasses should be in "test_pics" folder. 
+### Test files
 
-##How to run the code##
-Prior to running my code, please install the following libraries.
-- numpy
-- random
-- Image
-- os
-- re
+The pictures with sunglasses should be in "test_pics" folder.
 
-Also, Here is the way to run my code.
-- Theta is the threshold of the neuron activation.
-- Time is a parameter telling the steps of remembering the learned pictures. As the number of the steps increases, the remembered picture is more accurate.
-- size is the picture size in pixel. If you put a pic with different sizes, the code resize it.
-- threshold is the cutoff threshold to binarize 1 byte (0 to 255) brightness.
-- current_path should be current working folder path (usual way is os.getcwd())
+### Requirements
+
+Prior to running this program, please install the following libraries.
+
+-   `numpy`
+-   `PIL`
+
+### How to run the code
+
+-   Theta is the threshold of the neuron activation.
+-   epochs is a parameter telling the steps of remembering the learned pictures. As the number of the steps increases, the remembered picture is more accurate.
+-   size is the picture size in pixel. If you put a pic with different sizes, the code resize it.
+-   threshold is the cutoff threshold to binarize 1 byte (0 to 255) brightness.
+-   current_path should be current working folder path (usual way is os.getcwd())
 
 After you download all the files in this repository, please run "hopfield.py". Here is the main code.
+
+```python
+cwd_path = os.getcwd()
+
+# First, you can create a list of input file path
+train_path = os.path.join(cwd_path, 'train_pics')
+train_paths = [os.path.join(train_path, p) for p in os.listdir(train_path)]
+
+# Second, you can create a list of sungallses file path
+test_path = os.path.join(cwd_path, 'test_pics')
+test_paths = [os.path.join(test_path, p) for p in os.listdir(test_path)]
+
+# Hopfield network starts!
+hopfield(
+    train_files=train_paths,
+    test_files=test_paths,
+    theta=0.5, time=20000, size=(100, 100),
+    threshold=60, cwd_path=cwd_path
+)
 ```
-#First, you can create a list of input file path
-current_path = os.getcwd()
-train_paths = []
-path = current_path+"/train_pics/"
-for i in os.listdir(path):
-    if re.match(r'[0-9a-zA-Z-]*.jp[e]*g',i):
-        train_paths.append(path+i)
 
-#Second, you can create a list of sungallses file path
-test_paths = []
-path = current_path+"/test_pics/"
-for i in os.listdir(path):
-    if re.match(r'[0-9a-zA-Z-_]*.jp[e]*g',i):
-        test_paths.append(path+i)
+## Reference
 
-#Hopfield network starts!
-hopfield(train_files=train_paths, test_files=test_paths, theta=0.5,time=20000,size=(100,100),threshold=60, current_path = current_path)
-```
-
-
-## Reference ##
-- http://en.wikipedia.org/wiki/Hopfield_network
-- http://rishida.hatenablog.com/entry/2014/03/03/174331 (in Japanese)
-
-***
+-   http://en.wikipedia.org/wiki/Hopfield_network
+-   http://rishida.hatenablog.com/entry/2014/03/03/174331 (in Japanese)
--- hopfield.py
+++ hopfield.py
@@ -1,143 +1,160 @@
-#This is the sample code of discrere hopfield network
-
+import os
+import random
 
 import numpy as np
-import random
-import Image
-import os
-import re
 
-#convert matrix to a vector
-def mat2vec(x):
-    m = x.shape[0]*x.shape[1]
-    tmp1 = np.zeros(m)
-
-    c = 0
-    for i in range(x.shape[0]):
-        for j in range(x.shape[1]):
-            tmp1[c] = x[i,j]
-            c +=1
-    return tmp1
+from PIL import Image
 
 
-#Create Weight matrix for a single image
-def create_W(x):
-    if len(x.shape) != 1:
-        print "The input is not vector"
-        return
-    else:
-        w = np.zeros([len(x),len(x)])
-        for i in range(len(x)):
-            for j in range(i,len(x)):
-                if i == j:
-                    w[i,j] = 0
-                else:
-                    w[i,j] = x[i]*x[j]
-                    w[j,i] = w[i,j]
-    return w
+def readImg2array(file, size, threshold=145):
+    """Read Image file and convert it to Numpy array.
+
+    Arguments:
+        file {str} -- image local file path
+        size {tuple} -- resize image size
+
+    Keyword Arguments:
+        threshold {int} -- binary threshold (default: {145})
+
+    Returns:
+        np.ndarray -- image matrix
+    """
+
+    # read image file and convert it to gray
+    img = Image.open(file).convert(mode="L")
+    img = img.resize(size)  # resize image
+
+    # binarize image array
+    imgArray = np.asarray(img)
+    mat = np.zeros(img.size)
+
+    mat[imgArray > threshold] = 1
+    mat[imgArray <= threshold] = -1
+
+    return mat
 
 
-#Read Image file and convert it to Numpy array
-def readImg2array(file,size, threshold= 145):
-    pilIN = Image.open(file).convert(mode="L")
-    pilIN= pilIN.resize(size)
-    #pilIN.thumbnail(size,Image.ANTIALIAS)
-    imgArray = np.asarray(pilIN,dtype=np.uint8)
-    x = np.zeros(imgArray.shape,dtype=np.float)
-    x[imgArray > threshold] = 1
-    x[x==0] = -1
-    return x
+def array2img(data, outFile=None):
+    """Convert Numpy array to Image file like '*.jpg'.
 
-#Convert Numpy array to Image file like Jpeg
-def array2img(data, outFile = None):
+    Arguments:
+        data {np.ndarray} -- 1 or -1 matrix
 
-    #data is 1 or -1 matrix
-    y = np.zeros(data.shape,dtype=np.uint8)
-    y[data==1] = 255
-    y[data==-1] = 0
-    img = Image.fromarray(y,mode="L")
+    Keyword Arguments:
+        outFile {str} -- output file path (default: {None})
+
+    Returns:
+        Image.image -- imgage
+    """
+
+    mat = np.zeros(data.shape, dtype=np.uint8)
+    mat[data == 1] = 255
+    mat[data == -1] = 0
+    img = Image.fromarray(mat)
     if outFile is not None:
         img.save(outFile)
+
     return img
 
 
-#Update
-def update(w,y_vec,theta=0.5,time=100):
-    for s in range(time):
-        m = len(y_vec)
-        i = random.randint(0,m-1)
-        u = np.dot(w[i][:],y_vec) - theta
+def create_W(x):
+    """Create Weight matrix for a single image
 
+    Arguments:
+        x {np.ndarray} -- vector
+
+    Returns:
+        np.ndarray -- the weight of input(x)
+    """
+    # TODO: spend too time to do loop, anyway.
+    assert len(x.shape) == 1, "The input is not vector"
+
+    length = len(x)
+    w = np.zeros((length, length))
+    for i in range(length):
+        for j in range(i, length):
+            if i != j:
+                w[i, j] = x[i]*x[j]
+                w[j, i] = w[i, j]
+    return w
+
+
+def update(w, y_vec, theta=0.5, epochs=100):
+    length = len(y_vec)
+    for _ in range(epochs):
+        ind = random.randint(0, length-1)
+        u = np.dot(w[ind], y_vec) - theta
         if u > 0:
-            y_vec[i] = 1
+            y_vec[ind] = 1
         elif u < 0:
-            y_vec[i] = -1
+            y_vec[ind] = -1
 
     return y_vec
 
 
-#The following is training pipeline
-#Initial setting
-def hopfield(train_files, test_files,theta=0.5, time=1000, size=(100,100),threshold=60, current_path=None):
+def hopfield(train_files, test_files, theta=0.5, time=1000, size=(100, 100), threshold=60, cwd_path=None):
+    """Training pipeline.
 
-    #read image and convert it to Numpy array
-    print "Importing images and creating weight matrix...."
+    Arguments:
+        train_files {list} -- train data path list
+        test_files {list} -- test data path list
 
-    #num_files is the number of files
-    num_files = 0
+    Keyword Arguments:
+        theta {float} -- [description] (default: {0.5})
+        time {int} -- [description] (default: {1000})
+        size {tuple} -- [description] (default: {(100, 100)})
+        threshold {int} -- [description] (default: {60})
+        cwd_path {[type]} -- [description] (default: {None})
+    """
+
+    print("Loading images and creating weight matrix....")
+
+    # TODO: split read images and create weight
+    counter = 0  # num_files is the number of files
     for path in train_files:
-        print path
-        x = readImg2array(file=path,size=size,threshold=threshold)
-        x_vec = mat2vec(x)
-        print len(x_vec)
-        if num_files == 0:
+        x = readImg2array(file=path, size=size, threshold=threshold)
+        x_vec = x.flatten()
+        if counter == 0:
             w = create_W(x_vec)
-            num_files = 1
         else:
             tmp_w = create_W(x_vec)
             w = w + tmp_w
-            num_files +=1
+        counter += 1
 
-    print "Weight matrix is done!!"
+    print("Weight matrix is done!")
 
-
-    #Import test data
+    print("Imported test data.")
+    print("Updating...")
+    # TODO: split read images and create weight
     counter = 0
     for path in test_files:
-        y = readImg2array(file=path,size=size,threshold=threshold)
-        oshape = y.shape
-        y_img = array2img(y)
-        y_img.show()
-        print "Imported test data"
-
-        y_vec = mat2vec(y)
-        print "Updating..."
-        y_vec_after = update(w=w,y_vec=y_vec,theta=theta,time=time)
-        y_vec_after = y_vec_after.reshape(oshape)
-        if current_path is not None:
-            outfile = current_path+"/after_"+str(counter)+".jpeg"
-            array2img(y_vec_after,outFile=outfile)
-        else:
-            after_img = array2img(y_vec_after,outFile=None)
-            after_img.show()
-        counter +=1
+        y = readImg2array(file=path, size=size, threshold=threshold)
+        y_vec = y.flatten()
+        y_vec_after = update(w=w, y_vec=y_vec, theta=theta, epochs=time)
+        y_vec_after = y_vec_after.reshape(y.shape)
+        if cwd_path is not None:
+            outfile = os.path.join(cwd_path, 'recovery_'+str(counter)+".jpg")
+            array2img(y_vec_after, outFile=outfile)
+        counter += 1
 
 
-#Main
-#First, you can create a list of input file path
-current_path = os.getcwd()
-train_paths = []
-path = current_path+"/train_pics/"
-for i in os.listdir(path):
-    if re.match(r'[0-9a-zA-Z-]*.jp[e]*g',i):
-        train_paths.append(path+i)
+if __name__ == "__main__":
+    cwd_path = os.getcwd()
 
-#Second, you can create a list of sungallses file path
-test_paths = []
-path = current_path+"/test_pics/"
-for i in os.listdir(path):
-    if re.match(r'[0-9a-zA-Z-_]*.jp[e]*g',i):
-        test_paths.append(path+i)
+    # TODO: build hopfield class
 
-#Hopfield network starts!
-hopfield(train_files=train_paths, test_files=test_paths, theta=0.5,time=20000,size=(100,100),threshold=60, current_path = current_path)
+    # First, you can create a list of input file path
+    train_path = os.path.join(cwd_path, 'train_pics')
+    train_paths = [os.path.join(train_path, p) for p in os.listdir(train_path)]
+
+    # Second, you can create a list of sungallses file path
+    test_path = os.path.join(cwd_path, 'test_pics')
+    test_paths = [os.path.join(test_path, p) for p in os.listdir(test_path)]
+
+    # Hopfield network starts!
+    hopfield(
+        train_files=train_paths,
+        test_files=test_paths,
+        theta=0.5, time=20000, size=(100, 100),
+        threshold=60, cwd_path=cwd_path
+    )
